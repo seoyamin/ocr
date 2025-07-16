44d97d4a6ae55e6ef0dc2c1db0a5625047d9dff9
seoyamin <hyeminseo310@gmail.com>
2025-07-16 23:08:18 +0900

    init

---
+++ src/main/java/com/haru/cpn/CpnApplication.java
@@ -0,0 +1,13 @@
+package com.haru.cpn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haru/cpn/CpnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haru.cpn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
